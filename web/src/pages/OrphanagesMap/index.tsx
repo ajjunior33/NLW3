ba8e1bdbb6ed4933e4997d335876b7fc35cb1c03
André Souza <ajjunior33@gmail.com>
2020-10-17 19:50:32 -0300

    [UPDATE] Update no OrphangesMap.

--- web/src/pages/OrphanagesMap/index.tsx
+++ web/src/pages/OrphanagesMap/index.tsx
@@ -1,13 +1,21 @@
 import React from "react";
-import { FiPlus } from "react-icons/fi";
+import { FiPlus, FiArrowRight } from "react-icons/fi";
 import { Link } from "react-router-dom";
-import { Map, TileLayer } from "react-leaflet";
+import { Map, TileLayer, Marker, Popup } from "react-leaflet";
+import Leaflet from "leaflet";
 
 import "leaflet/dist/leaflet.css";
 
 // files
 import "../../styles/pages/orphanagesMap.css";
 import mapMarkerImg from "../../images/map-marker.svg";
+
+const mapIcon = Leaflet.icon({
+  iconUrl: mapMarkerImg,
+  iconSize: [50, 56],
+  iconAnchor: [25, 56],
+  popupAnchor: [170, 2],
+});
 
 function OrphanagesMap() {
   return (
@@ -29,9 +37,24 @@ function OrphanagesMap() {
         style={{ width: "100%", height: "100%" }}
       >
         {/* <TileLayer url="http://a.tile.openstreetmap.org/{z}/{x}/{y}.png" /> */}
-        <TileLayer url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}/>
+        <TileLayer
+          url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
+        />
+        <Marker position={[-20.3475549, -40.3802901]} icon={mapIcon}>
+          <Popup
+            closeButton={false}
+            minWidth={240}
+            maxWidth={240}
+            className="map-popup"
+          >
+            Lar das meninas
+            <Link to="/orphanages/1">
+              <FiArrowRight color="#FFF" size={20} />
+            </Link>
+          </Popup>
+        </Marker>
       </Map>
-      <Link to="" className="create-orphanage">
+      <Link to="/orphanages/create" className="create-orphanage">
         <FiPlus size={32} color="#fff" />
       </Link>
     </div>
