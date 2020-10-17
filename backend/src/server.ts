7462e4c4322c51002337946e20fbd77948566c1f
André Souza <ajjunior33@gmail.com>
2020-10-17 19:15:40 -0300

    [UPDATE] Instance Cors.

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import path from "path";
+import cors from "cors";
 import "express-async-errors";
 
 import routes from "./routes";
@@ -10,6 +11,7 @@ import "./database/connection";
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 app.use("/uploads", express.static(path.join(__dirname, "..", "uploads")));
